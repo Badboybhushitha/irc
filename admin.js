1f0538024a233d5f7ea3ef306840af0934da40fe
B.Arunod <103347448+Badboybhushitha@users.noreply.github.com>
2024-11-13 16:01:23 +0530

    admin.js

--- admin.js
+++ admin.js
@@ -26,7 +26,7 @@ function addNewUser() {
     localStorage.setItem("users", JSON.stringify(users));
 
     alert("New user added!");
-    loadUsers();  // Refresh the user list
+    loadUsers();
 }
 
 function loadUsers() {
@@ -42,10 +42,6 @@ function loadUsers() {
                 <p><strong>Email:</strong> ${user.email}</p>
                 <p><strong>Username:</strong> ${user.username}</p>
                 <p><strong>Password:</strong> ${user.password}</p>
-                <p><strong>Bank Name:</strong> ${user.bankName}</p>
-                <p><strong>Account Number:</strong> ${user.accountNumber}</p>
-                <p><strong>Account Name:</strong> ${user.accountName}</p>
-                <p><strong>Branch:</strong> ${user.branch}</p>
                 <button onclick="deleteUser(${index})">Delete</button>
                 <hr>
             </div>
@@ -59,7 +55,7 @@ function deleteUser(index) {
     localStorage.setItem("users", JSON.stringify(users));
 
     alert("User deleted!");
-    loadUsers();  // Refresh the user list
+    loadUsers();
 }
 
 // Load users when admin page is loaded
